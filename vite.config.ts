d5f54d2d72b95c31e55edc179c2a2167c2e84d4c
Leonardo Torrico <leonardotorrico@grupovenado.com>
2026-09-04 08:06:29 -0400

    fix(config): correct port number in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "node:path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: Number(process.env.PORT) || 5173,
+    port: Number(process.env.PORT) || 5175,
     strictPort: true,
   },
-})
+});
